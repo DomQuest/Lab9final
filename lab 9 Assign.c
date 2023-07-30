5cef250d62cdd34254a33cead53819d6d92a5357
Dominick Carter <Dominickcarter23@gmail.com>
2023-07-30 18:57:07 -0400

    first commit

---
+++ lab 9 Assign.c
@@ -0,0 +1,133 @@
+
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// RecordType
+struct RecordType
+{
+	int		id;
+	char	name;
+	int		order; 
+};
+
+// Fill out this structure
+struct HashType
+{
+    struct RecordType** pRecords;
+    int size;
+    int count;
+
+};
+
+// Compute the hash function
+int hash(int x)
+{
+    return x % 100;
+}
+
+// parses input file to an integer array
+int parseData(char* inputFileName, struct RecordType** ppData)
+{
+	FILE* inFile = fopen(inputFileName, "r");
+	int dataSz = 0;
+	int i, n;
+	char c;
+	struct RecordType *pRecord;
+	*ppData = NULL;
+
+	if (inFile)
+	{
+		fscanf(inFile, "%d\n", &dataSz);
+		*ppData = (struct RecordType*) malloc(sizeof(struct RecordType) * dataSz);
+		// Implement parse data block
+		if (*ppData == NULL)
+		{
+			printf("Cannot allocate memory\n");
+			exit(-1);
+		}
+		for (i = 0; i < dataSz; ++i)
+		{
+			pRecord = *ppData + i;
+			fscanf(inFile, "%d ", &n);
+			pRecord->id = n;
+			fscanf(inFile, "%c ", &c);
+			pRecord->name = c;
+			fscanf(inFile, "%d ", &n);
+			pRecord->order = n;
+		}
+
+		fclose(inFile);
+	}
+
+	return dataSz;
+}
+
+// prints the records
+void printRecords(struct RecordType pData[], int dataSz)
+{
+	int i;
+	printf("\nRecords:\n");
+	for (i = 0; i < dataSz; ++i)
+	{
+		printf("\t%d %c %d\n", pData[i].id, pData[i].name, pData[i].order);
+	}
+	printf("\n\n");
+}
+
+// display records in the hash structure
+// skip the indices which are free
+// the output will be in the format:
+// index x -> id, name, order -> id, name, order ....
+void displayRecordsInHash(struct HashType *pHashArray, int hashSz)
+{
+	int i;
+
+	for (i=0;i<hashSz;++i)
+	{
+		// if index is occupied with any records, print all
+		if (pHashArray->pRecords[i] != NULL){
+		    printf("Index %d -> %d %c %d\n", i, pHashArray->pRecords[i]->id, pHashArray->pRecords[i]->name, pHashArray->pRecords[i]->order);
+		}
+	}
+	printf("\n");
+}
+
+int main(void)
+{
+	struct RecordType *pRecords;
+	int recordSz = 0;
+
+	recordSz = parseData("input.txt", &pRecords);
+	printRecords(pRecords, recordSz);
+	// Your hash implementation
+	/*int key = hash(record->id);
+
+    // Handle collision using separate chaining (linked list)
+    if (hashTable->records[key] == NULL)
+    {
+        hashTable->records[key] = record;
+    }
+    else
+    {
+        struct RecordType* current = hashTable->records[key];
+        while (current->next != NULL)
+        {
+            current = current->next;
+        }
+        current->next = record;
+    }
+
+    hashTable->count++;
+}*/
+
+	
+	
+}
